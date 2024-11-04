718e5624c864722e909eea6b6df1e1e40e12e345
yechen17 <316552575@qq.com>
2024-11-04 19:32:57 +0800

    update plugin

--- client/front/src/main.js
+++ client/front/src/main.js
@@ -1,4 +1,21 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import 'vuetify/styles'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
-createApp(App).mount('#app')
+library.add(fas)
+
+const vuetify = createVuetify({
+    components,
+    directives,
+    icons: {
+        defaultSet: 'mdi',
+    }
+})
+
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).use(vuetify).mount('#app')
